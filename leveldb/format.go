156ccacb37b200a0aba63e2fadc091422058ed88
caodhuan <cao.d.huan@gmail.com>
2019-01-03 12:38:40 +0800

    change: misc

--- leveldb/format.go
+++ leveldb/format.go
@@ -1,6 +1,41 @@
 package leveldb
 
+import "encoding/binary"
+// Maximum encoding length of a BlockHandle
+const kMaxEncodedLength = 10 + 10
+
+type BlockContents struct {
+	data []byte
+	cachable bool
+	heapAllocated bool
+}
+
 type BlockHandle struct {
 	offset uint64
 	size uint64
+}
+
+func (this *BlockHandle) EncodeTo(dst []byte) {
+	binary.PutUvarint(dst, this.offset)
+	binary.PutUvarint(dst, this.size)
+	
+}
+
+func (this *BlockHandle) DecodeFrom(input []byte) Status {
+	value, n := binary.Uvarint(input)
+	if n > 0 {
+		this.offset = value
+	} else {
+		return Corruption("bad block handle")
+	}
+
+	value, n = binary.Uvarint(input[n:])
+
+	if n > 0 {
+		this.size = value
+	} else {
+		Corruption("bad block handle")
+	}
+
+	return OK()	
 }
--- leveldb/table.go
+++ leveldb/table.go
@@ -14,7 +14,25 @@ type Table struct {
 }
 
 func BlockReader(arg interface{}, options *ReadOptions, indexValue string) Iterator {
+	table, _ := arg.(*Table)
+	blockCache := table.options.BlockCache
+	var (block Block
+		 Handle interface{}
+		 handle BlockHandle
+		)
+	input := indexValue
+
+	s := handle.DecodeFrom([]byte(input) )
+	// We intentionally allow extra stuff in index_value so that we
+	// can add more features in the future.
+	if s.OK() {
+		var contents BlockContents
+		if blockCache != nil {
+			var cacheKeyBuffer [16]byte
+		}
+	}
 	// todo
+
 	return NewEmptyIterator()
 }
 
